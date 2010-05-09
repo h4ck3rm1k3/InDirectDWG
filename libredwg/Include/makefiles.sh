437512bf77fd044c0d7d2c01977ec06f24c971b7
James Michael DuPont <mdupont@introspector-mail.(none)>
2010-05-09 13:04:01 +0200

    making progress

---
+++ libredwg/Include/makefiles.sh
@@ -0,0 +1,8 @@
+touch Ge/GeExtents3d.h
+touch DbViewportTable.h
+touch DbViewportTableRecord.h
+touch DbBlockTable.h
+touch DbBlockTableRecord.h
+touch DbCircle.h
+touch DbLayerTable.h
+touch DbLayerTableRecord.h
